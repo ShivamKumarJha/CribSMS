78a4b63e6f1f9d2b37061e7d92fb9ca0daa23412
ShivamKumarJha <jha.shivam3@gmail.com>
2021-09-14 16:05:42 +0530

    Navigation: AppBarConfiguration
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/cribsms/MainActivity.kt
+++ app/src/main/java/com/shivamkumarjha/cribsms/MainActivity.kt
@@ -2,22 +2,36 @@ package com.shivamkumarjha.cribsms
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private val toolbar: Toolbar by lazy {
+        findViewById(R.id.toolbar)
+    }
+
     //Navigation
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Toolbar
+        toolbar.title = title
+        setSupportActionBar(toolbar)
+
         //AppBar
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
